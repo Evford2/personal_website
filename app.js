eab4195fd69ec990e99b5c62298301bc2e725673
Evan Ford <98207065+Evford2@users.noreply.github.com>
2023-05-24 09:55:19 -0500

    added stuff

---
+++ app.js
@@ -0,0 +1,38 @@
+const sections = document.querySelectorAll('.section');
+const sectbtns = document.querySelectorAll('.controls');
+const sectbtn = document.querySelectorAll('.control');
+const allSections = document.querySelectorAll('.main-content')[0];
+
+function PageTransitions(){
+    //Button click active class
+    for(let i = 0; i < sectbtn.length; i++){
+        sectbtn[i].addEventListener('click', function(){
+            let currentBtn = document.querySelectorAll('.active-btn');
+            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
+            this.className += ' active-btn';
+        })
+    }
+
+    //sections active class
+    allSections.addEventListener('click',(e) =>{
+        console.log(e.target);
+        const id = e.target.dataset.id;
+        if(id){
+            //remove active tag from other buttons
+            sectbtns.forEach((btn) =>{
+                btn.classList.remove('active')
+            })
+            e.target.classList.add('active')
+
+            //hide other sections
+            sections.forEach((section) =>{
+                section.classList.remove('active')
+            })
+
+            const element = document.getElementById(id);
+            element.classList.add('active');
+        }
+    })
+}
+
+PageTransitions();
